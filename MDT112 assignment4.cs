77d6030ad2912a52f97def83a3630dbfb3d9e37f
Palerider2233 <61342186+Palerider2233@users.noreply.github.com>
2020-04-10 23:21:06 +0700

    Add files via upload

---
+++ MDT112 assignment4.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MDT112_assignment4
+{
+    public class Shooting : MonoBehaviour
+    {
+        public GameObject bulletPosition;
+        public GameObject bullet;
+        public float speed = 10000f;
+         //Start is called before the first frame update
+         void Start()
+        {
+
+        }
+
+        //Update is called once per frame
+        void Update()
+        {
+            if(Input.GetButtonDown("firel"))
+            {
+                GameObject instBullet = Instaniate(bullet, bulletPosiyion.Tranform.position, bulletPostion.tranform.rotation) as GameObject;
+                Rigidbody intBulletRigidbody = instBullet.GetComponent<tRigidbody>();
+                
+                instBulletRigidbody.AddForce(bulletPosition.TranForm.forward * speed);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.Equals())
+            {
+                Debug.Log("You Win");
+            }
+        }
+    }
+}
